15b6eb163d7d0ab0fbabee7bde841d339a5b491c
Johnnie Menges Giacomelli <johnniegiacomelli@gmail.com>
2023-05-31 16:33:00 -0300

    feat: Adicionado botões com space_between

--- site/v2/index.js
+++ site/v2/index.js
@@ -5,4 +5,18 @@ function abrirRota() {
     var url = `https://www.google.com/maps/dir/?api=1&destination=${destinoLat},${destinoLng}`;
     window.open(url, "_blank");
   }
-  
+  /*Carrossel Destaque*/
+  $(document).ready(function(){
+    $('.carrossel').slick({
+      autoplay: true, // Inicia a rotação automática
+      autoplaySpeed: 5000, // Tempo de exibição de cada imagem (em milissegundos)
+      arrows: true, // Exibe as setas de navegação
+      dots: false, // Esconde os indicadores de pontos
+      infinite: true, // Navegação infinita
+      speed: 500, // Velocidade da animação
+      slidesToShow: 1, // Quantidade de slides exibidos por vez
+      slidesToScroll: 1, // Quantidade de slides a serem movidos
+      prevArrow: $('.bt_Anterior'),
+      nextArrow: $('.bt_Proximo')
+    });
+  });
